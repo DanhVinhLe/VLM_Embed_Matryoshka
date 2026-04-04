0b1e74bb5dfe4986bd81c6c4df5b17ee8bbb401d
Ubuntu <gdi-user@atlmartin07.geodigital.com>
2026-04-04 06:31:33 +0000

    new_scripts

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 python eval_mmeb.py \
     --model_name ./training/AdaptiveMRL_fastVLM_combine/checkpoint-epoch-1 \
-    --encode_output_path ./MMEB-evaloutputs/fastvlm_adaptive_mrl_combine/ \
+    --encode_output_path ./MMEB-evaloutputs/fastvlm_adaptive_mrl_combine2/ \
     --pooling eos \
     --model_backbone "llava_qwen2" \
     --normalize True \
